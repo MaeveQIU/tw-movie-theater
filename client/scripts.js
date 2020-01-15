87c2f24402855f4c2cae14a81e44f89e4d6bcc4c
Maeve QIU <maeve.cs23@gmail.com>
2020-01-15 17:58:13 +0800

    [Shi QIU]{feat}: Complete the search results page

--- client/scripts.js
+++ client/scripts.js
@@ -1,4 +1,3 @@
-
 const getData = (start, count) => {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", `http://127.0.0.1:8888/v2/movie/top250?start=${start}&count=${count}`, false);
@@ -10,7 +9,7 @@ const getData = (start, count) => {
 
 const movieList = getData(0, 40).subjects;
 
-const getGenre = (movieList) => {
+const getGenre = movieList => {
   let newList = [];
   movieList.forEach(element => {
     element.genres.forEach(item => {
@@ -22,7 +21,7 @@ const getGenre = (movieList) => {
   return newList;
 }
 
-const renderGenre = (genreList) => {
+const renderGenre = genreList => {
   genreList.forEach(element => {
     const newGenre = document.createElement("li");
     newGenre.setAttribute("id", `${element}`);
@@ -32,9 +31,7 @@ const renderGenre = (genreList) => {
   })
 }
 
-renderGenre(getGenre(movieList));
-
-const renderMovie = (movieList) => {
+const renderMovie = movieList => {
   movieList.forEach(element => {
     const newMovie = document.createElement("div");
     newMovie.innerHTML = `
@@ -45,31 +42,61 @@ const renderMovie = (movieList) => {
   });
 }
 
-renderMovie(movieList);
-
-const clearList = () => {
-  document.getElementById("movies").innerHTML = "";
+const clearList = node => {
+  document.getElementById(node).innerHTML = "";
 }
 
 const filterList = (movieList, genre) => {
   return movieList.filter(element => element.genres.includes(genre));
 }
 
+const checkResult = movieList => {
+  const input = document.getElementById("search-area").value;
+  return movieList.filter(element => element.title.includes(input));
+}
 
+const displayResult = resultArr => {
+  if (resultArr.length !== 0) {
+    resultArr.forEach(element => {
+      const result = document.createElement("div");
+      result.setAttribute("class", "movie-result")
+      result.innerHTML = `
+      <img src=${element.images.small}>
+      <p class="title">${element.title}</p>
+      <p>Rating: ${element.rating.average}</p>
+      <p>Year: ${element.year}</p>
+      <p>Directors: ${element.directors.map(item => item.name_en).join(", ")}</p>
+      <p>Casts: ${element.casts.map(item => item.name_en).join(", ")}</p>
+      <p>Duration: ${element.durations[0]}`
+      document.getElementById("main").appendChild(result);
+    });
+  }
+  else {
+    const line = document.createElement("div");
+    line.setAttribute("class", "no-result")
+    line.innerHTML = `
+    <p>Sorry we can't find the movie you're searching for.<br>
+    But we're constantly updating our database.<br>
+    So stay tuned! ❤︎</p>`
+    document.getElementById("main").appendChild(line);
+  }
+}
 
 const addEvents = () => {
   document.addEventListener("click", event => {
     if(event.target.className === "all-list") {
-      clearList();
+      clearList("movies");
       renderMovie(movieList);
     }
     if(event.target.className === "genre-list") {
       let target = filterList(movieList, event.target.id);
-      clearList();
+      clearList("movies");
       renderMovie(target);
+    }
+    if(event.target.id === "search-button") {
+      clearList("main")
+      displayResult(checkResult(movieList));
     }
 
   });
 }
-
-addEvents();
